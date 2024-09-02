35295483ef482523ad7814921ca3b00fc21f4cc8
Jonathan <jonathanchang96@gmail.com>
2024-09-02 22:11:38 +0700

    custom dropdown component

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -1,22 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Metadata } from "next";
-import { PageHeader } from "@/components/PageHeader";
-import { getInsightList } from "@/services/insight";
-import { ButtonPrimary } from "@/components/Button";
-import { InsightCard } from "@/components/InsightCard";
-import { customConfig } from "../../../config";
-import { Option, Select } from "@material-tailwind/react";
-import { InsightList } from "@/components/Page/InsightList";
-
+import React, { useEffect, useState } from 'react'
+import { Metadata } from 'next'
+import { PageHeader } from '@/components/PageHeader'
+import { getInsightList } from '@/services/insight'
+import { ButtonPrimary } from '@/components/Button'
+import { InsightCard } from '@/components/InsightCard'
+import { customConfig } from '../../../config'
+import { InsightList } from '@/components/Page/InsightList'
 
 export default async function Insight({ params }: any) {
- 
-    return <InsightList />;
+  return <InsightList />
 }
 
 export const metadata: Metadata = {
-    title: "Mores | Under Construction",
-};
+  title: 'Mores | Under Construction',
+}
 
 export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
--- src/components/Page/InsightList/index.tsx
+++ src/components/Page/InsightList/index.tsx
@@ -1,62 +1,88 @@
 'use client'
-import { InsightCard } from "@/components/InsightCard";
-import { PageHeader } from "@/components/PageHeader";
-import { getInsightList } from "@/services/insight";
-import { TInsight } from "@/types/insight";
-import { Option, Select } from "@material-tailwind/react";
-import { ReactNode, useEffect, useState } from "react";
-import { customConfig } from "../../../../config";
-import { ButtonPrimary } from "@/components/Button";
+import { InsightCard } from '@/components/InsightCard'
+import { PageHeader } from '@/components/PageHeader'
+import { getInsightList } from '@/services/insight'
+import { TInsight } from '@/types/insight'
+import { ReactNode, useEffect, useState } from 'react'
+import { customConfig } from '../../../../config'
+import { ButtonPrimary } from '@/components/Button'
+import { Select } from '@/components/Select'
+import { Option } from '@/components/Option'
 
 export const InsightList = () => {
-    const [insights, setInsights] = useState<TInsight[]>()
-    const [page, setPage] = useState(1)
-    const [perPage, setPerPage] = useState(4)
+  const [insights, setInsights] = useState<TInsight[]>()
+  const [page, setPage] = useState(1)
+  const [perPage, setPerPage] = useState(4)
 
-    useEffect(() => {
-        const fetchInsights = async () => {
-            const insightsRes = await getInsightList({}, page, perPage)
-            const data = insightsRes?.items || []
-            const prevInsight = insights || []
-            setInsights([...prevInsight, ...data])
-        }
-        fetchInsights()
-    }, [page])
+  useEffect(() => {
+    const fetchInsights = async () => {
+      const insightsRes = await getInsightList({}, page, perPage)
+      const data = insightsRes?.items || []
+      const prevInsight = insights || []
+      setInsights([...prevInsight, ...data])
+    }
+    fetchInsights()
+  }, [page])
 
-    return <section className="flex flex-col">
-        <section>
-            <PageHeader title="INSIGHT" background="/images/bg-insights.png" />
-        </section>
-        <section className="px-120 py-100 flex flex-col gap-48">
-            <div>
-                {/* FILTER */}
+  return (
+    <section className="flex flex-col">
+      <section>
+        <PageHeader title="INSIGHT" background="/images/bg-insights.png" />
+      </section>
+      <section className="px-120 py-100 flex flex-col gap-48">
+        <div className="grid grid-cols-3 gap-24">
+          <Select>
+            <Option value="">Featured Insight</Option>
+          </Select>
+          <Select>
+            <Option value="">Industries</Option>
+          </Select>
+          <Select>
+            <Option value="">Services</Option>
+          </Select>
+        </div>
+        <div className="flex flex-col ">
+          <h1 className="font-semibold text-2xl text-black font-inter pb-48">
+            Most Recent Insights
+          </h1>
+          <div className="grid grid-cols-4 gap-24 -ml-24">
+            {insights?.map((insight, index) => {
+              const insightIndusties = insight.expand?.industry_tags || []
+              const subTitle =
+                insightIndusties.length > 0 ? insightIndusties[0].name : ''
 
-
-            </div>
-            <div className="flex flex-col ">
-                <h1 className="font-semibold text-2xl text-black font-inter pb-48">Most Recent Insights</h1>
-                <div className="grid grid-cols-4 gap-24 -ml-24">
-                    {insights?.map((insight, index) => {
-                        const insightIndusties = insight.expand?.industry_tags || []
-                        const subTitle =
-                            insightIndusties.length > 0 ? insightIndusties[0].name : ''
-
-                        // Change this if grid-cols-change ex: window.innerWidth >= 768 ? 5 : 4
-                        let gridCols = 4
-                        const isFirstRow = (index + 1) % gridCols == 1
-                        return <div key={insight.id} className={`${!isFirstRow ? 'border-l border-gray-silver' : ''} pb-36 pl-24`}>
-                            <InsightCard
-                                image={`${customConfig.POCKETBASE_FILE_URL}/insights/${insight.id}/${insight.button_image}`}
-                                title={insight.title}
-                                description={insight.summary}
-                                subtitle={subTitle}
-                                path={`/insights/${insight.id}`} />
-                        </div>
-                    })}
-
+              // Change this if grid-cols-change ex: window.innerWidth >= 768 ? 5 : 4
+              let gridCols = 4
+              const isFirstRow = (index + 1) % gridCols == 1
+              return (
+                <div
+                  key={insight.id}
+                  className={`${
+                    !isFirstRow ? 'border-l border-gray-silver' : ''
+                  } pb-36 pl-24`}
+                >
+                  <InsightCard
+                    image={`${customConfig.POCKETBASE_FILE_URL}/insights/${insight.id}/${insight.button_image}`}
+                    title={insight.title}
+                    description={insight.summary}
+                    subtitle={subTitle}
+                    path={`/insights/${insight.id}`}
+                  />
                 </div>
-                <div className="flex justify-center mt-64"><ButtonPrimary onClick={() => { setPage(page + 1) }}>MORE</ButtonPrimary></div>
-            </div>
-        </section>
+              )
+            })}
+          </div>
+          <div className="flex justify-center mt-64">
+            <ButtonPrimary
+              onClick={() => {
+                setPage(page + 1)
+              }}
+            >
+              MORE
+            </ButtonPrimary>
+          </div>
+        </div>
+      </section>
     </section>
+  )
 }
