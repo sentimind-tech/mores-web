63302267d2db54be0cf4af4cf30e59171aa5dfb1
Jonathan <jonathanchang96@gmail.com>
2024-10-21 18:56:50 +0700

    fix header

--- src/components/SectionHeader/index.tsx
+++ src/components/SectionHeader/index.tsx
@@ -6,9 +6,9 @@ type TSectionHeaderProp = {
 export const SectionHeader = ({ title }: TSectionHeaderProp) => {
     return <div className="section-header-container">
         <div className="flex items-center w-full gap-48">
-            <div className="flex-grow w-full h-[1px] border-t border-gray-silver"></div>
+            <div className="hidden md:flex flex-grow w-full h-[1px] border-t border-gray-silver"></div>
             <div className="whitespace-nowrap font-supplymono text-lg lg:text-2xl text-black font-normal">{title}</div>
-            <div className="flex-grow w-full h-[1px] border-t border-gray-silver"></div>
+            <div className="hidden md:flex flex-grow w-full h-[1px] border-t border-gray-silver"></div>
         </div>
 
     </div>
